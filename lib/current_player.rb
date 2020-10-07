5a979abd9fa865d99a39c583279e7d00c94f2cd8
carefree-window-8613 <jakobep24@gmail.com>
2020-10-07 22:46:37 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,2 +1,11 @@
-def turn_count(board)
+board1 = ["O", " ", " ", " ", "X", " ", " ", " ", "X"]
+board2 = ["O", " ", " ", " ", "X", " ", " ", "O", "X"]
+
+def turn_count(board1)
+  counter = 0
+  board1.each do |occupations|
+    if occupations < 9
+      counter += 1
+    end
+    return counter
 end
