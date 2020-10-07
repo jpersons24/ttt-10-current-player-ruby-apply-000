5569ba733bb8ef93afdd6518d057a7090df8a557
carefree-window-8613 <jakobep24@gmail.com>
2020-10-07 22:36:21 +0000

    Automatically backed up by Learn

---
+++ lib/current_player.rb
@@ -0,0 +1,10 @@
+board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+
+def turn_count(board)
+  counter = 0
+  board.each do |space|
+    if |space| != (" ")
+      counter += 1
+  end
+  return counter
+end
