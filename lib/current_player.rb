adb3d08e5d1bcb0d1a244b73f9dda8c8f00725ea
carefree-window-8613 <jakobep24@gmail.com>
2020-10-07 22:37:06 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -3,7 +3,7 @@ board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 def turn_count(board)
   counter = 0
   board.each do |space|
-    if |space| != (" ")
+    if space != (" ")
       counter += 1
   end
   return counter
